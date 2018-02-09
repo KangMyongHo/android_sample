4b7ddf2b082ba61703f4c8c1653fbe18d7b5dc57
Myong-Ho Kang <mhkang@futurewiz.co.kr>
2018-02-09 13:56:20 +0900

    코틀린 적용 프로젝트 생성

---
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by myong-hokang on 2018. 2. 9..
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
