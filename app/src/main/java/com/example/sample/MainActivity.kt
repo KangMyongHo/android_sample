0b941fac26299b89498d7a2e1558c8ca6f74889a
Myong-Ho Kang <mhkang@futurewiz.co.kr>
2018-02-23 11:02:22 +0900

    기본 툴바 적용

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
 
 /**
  * Created by myong-hokang on 2018. 2. 9..
@@ -12,5 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+
+
     }
 }
